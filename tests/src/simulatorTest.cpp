aa43f1bcf1d67bfdd0f57fb0eb9ed6096461962d
TarasKoval <antares13.1998@gmail.com>
2018-08-20 16:30:19 +0300

    stable 1.0

--- tests/src/simulatorTest.cpp
+++ tests/src/simulatorTest.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 #include "../include/mockSimulator.h"
+#include "controller.h"
 
 using ::testing::AtLeast;
 using ::testing::Return;
@@ -12,4 +13,33 @@ TEST(SimulatorTest, CallSetInteriorControl) {
             .WillRepeatedly(Return(true));
 
     EXPECT_TRUE(simulator.setInteriorControl(1));
+}
+
+class ControllerTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        simulator = new MockSimulator;
+    }
+
+    void TearDown() override {
+        delete simulator;
+    }
+
+    MockSimulator *simulator;
+    std::vector<int> temperature15{14, 16, 17, 13, 15};
+    std::vector<int> temperature17{18, 16, 21, 13, 17};
+};
+
+TEST_F(ControllerTest, DifferenceCalculation) {
+
+    EXPECT_CALL(*simulator, getOutdoorTemperatureLog(testing::_, testing::_))
+            .Times(1)
+            .WillOnce(testing::SetArgReferee<0>(temperature17));
+    EXPECT_CALL(*simulator, getIndoorTemperatureLog(testing::_, testing::_))
+            .Times(1)
+            .WillOnce(testing::SetArgReferee<0>(temperature15));
+    EXPECT_CALL(*simulator, setInteriorControl(2)).Times(1);
+
+    Controller controller(simulator);
+    controller.minimizeTemperatureDifference();
 }
