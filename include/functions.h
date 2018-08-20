f3e4d371b6fb8b6be4f8c494c3f353a62df0bdff
TarasKoval <antares13.1998@gmail.com>
2018-08-20 14:46:04 +0300

    deleted unused functions

--- include/functions.h
+++ include/functions.h
@@ -10,24 +10,4 @@
 
 double arithmeticMean(const std::vector<int> &inputData);
 
-double mathExpectation(const std::vector<ulong> &inputData, unsigned long gapPosition);
-
-double mathExpectation(const std::vector<ulong> &inputData);
-
-double squaredDispersionCalc(const std::vector<ulong> &inputData, unsigned long gapPosition);
-
-double squaredDispersionCalc(const std::vector<ulong> &inputData);
-
-double studentsDistribution(const std::vector<ulong> &inputData, unsigned long gapPosition);
-
-void getRidOfRoughErrors(std::vector<ulong> &inputData);
-
-bool checkEqualityOfDistributionCenters(const std::vector<ulong> &inputData,
-                                        const std::vector<ulong> &perfectData);
-
-bool differenceByAccidentalReasons(const std::vector<ulong> &inputData,
-                                   const std::vector<ulong> &perfectData);
-
-bool fullCheck(const std::vector<ulong> &perfectData, const std::vector<ulong> &inputData);
-
 #endif //RRORELCALC_FUNCTIONS_H
